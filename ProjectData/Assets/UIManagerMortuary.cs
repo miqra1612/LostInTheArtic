c4eaf0563ad23f8b38b33aa327b3365c8c009b00
Rama <miqra1612@gmail.com>
2021-10-21 22:00:10 +0700

    - adding script

---
+++ ProjectData/Assets/UIManagerMortuary.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManagerMortuary : MonoBehaviour
+{
+    public GameData gameData;
+    public SceneController sceneController;
+
+    [Header("This area is for button clue")]
+    public Button clueBtn1;
+
+    [Header("This area is for non puzzle clue panel")]
+    public RectTransform clue1;
+
+    [Header("Check this variable if this is the last puzzle room in the game")]
+    public bool lastRoom = false;
+    
+    public float tweenDelay = 1f;
+    public List<RectTransform> openWindow = new List<RectTransform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find game data object in the begining of the scene
+        gameData = GameObject.FindGameObjectWithTag("data").GetComponent<GameData>();
+
+        clueBtn1.onClick.AddListener(() => NonPuzzleClue(clue1));
+    }
+    
+    // This is a fungtion to activate a clue window 3
+    public void NonPuzzleClue(RectTransform panel)
+    {
+        ClosePanel();
+        OpenPanel(panel);
+    }
+
+    void OpenPanel(RectTransform rt)
+    {
+        rt.DOAnchorPos(Vector2.zero, tweenDelay);
+        openWindow.Add(rt);
+        gameData.innerMortuaryPuzzleOpen[0] = true;
+    }
+
+    public void ClosePanel()
+    {
+        for (int i = 0; i < openWindow.Count; i++)
+        {
+            openWindow[i].DOAnchorPos(new Vector2(0, 2000), tweenDelay);
+        }
+
+        openWindow.Clear();
+    }
+}
